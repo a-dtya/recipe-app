f4b145364915cf77582f0b4a764d6901312f3d49
Adithya V <adithyavinod1943@gmail.com>
2023-05-17 13:42:08 +0530

    bug fix in home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import axios from 'axios'
 function Home() {
-    const [recipe,setRecipe]=useState([])
+    const [recipe,setRecipe]=useState()
     useEffect(()=>{
         const fetchRecipe = async()=>{
             try {
