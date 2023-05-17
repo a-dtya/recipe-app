91d2b683cc00b98ef0168f2030f0b0efe6275cc5
Adithya V <adithyavinod1943@gmail.com>
2023-05-17 10:00:08 +0530

    Initialized Home Page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,18 @@
-import React from 'react'
-
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
 function Home() {
+    const [recipe,setRecipe]=useState([])
+    useEffect(()=>{
+        const fetchRecipe = async()=>{
+            try {
+                const response = await axios.get("http://localhost:3001/recipe")
+                setRecipe(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+            
+        }
+    },[])
   return (
     <>
 
